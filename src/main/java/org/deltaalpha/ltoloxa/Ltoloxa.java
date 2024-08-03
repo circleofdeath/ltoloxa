77416f6660a2179bf956c4c517b9e9916c145fa4
circleofdeath <deltaalphaengi@proton.me>
2024-08-03 16:50:53 +0300

    basics

---
+++ src/main/java/org/deltaalpha/ltoloxa/Ltoloxa.java
@@ -0,0 +1,20 @@
+package org.deltaalpha.ltoloxa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ltoloxa {
+    public Map<String, String> map = new HashMap<>();
+
+    public void set(String key, String value) {
+        map.put(key, value);
+    }
+
+    public String get(String key) {
+        if(key == null || !map.containsKey(key)) {
+            throw new IllegalStateException("Value not found or key is null");
+        }
+
+        return map.get(key);
+    }
+}

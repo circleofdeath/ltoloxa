bbdd54711724619cefc64e54d4087ac15d5259d8
circleofdeath <deltaalphaengi@proton.me>
2024-08-03 17:35:24 +0300

    Default instructions

---
+++ src/main/java/org/deltaalpha/ltoloxa/instructions/LtoloxaInstructions.java
@@ -0,0 +1,37 @@
+package org.deltaalpha.ltoloxa.instructions;
+
+import org.deltaalpha.ltoloxa.LtoloxaEnv;
+
+public class LtoloxaInstructions {
+    public static class DirMoveInstruction implements ILtoloxaInstruction {
+        public String input;
+
+        @Override
+        public void exec(LtoloxaEnv env) {
+            env.append(env.format(input));
+        }
+    }
+
+    public static class DirDownInstruction implements ILtoloxaInstruction {
+        @Override
+        public void exec(LtoloxaEnv env) {
+            env.prevdir();
+        }
+    }
+
+    public static class SetInstruction implements ILtoloxaInstruction {
+        public String key, value;
+
+        @Override
+        public void exec(LtoloxaEnv env) {
+            env.handle(env.format(key), env.format(value));
+        }
+    }
+
+    public static class VareditInstruction implements ILtoloxaInstruction {
+        @Override
+        public void exec(LtoloxaEnv env) {
+            env.varedit = !env.varedit;
+        }
+    }
+}
